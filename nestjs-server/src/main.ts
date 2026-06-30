11385996c764cdecc6cd6ae4119f04274625fb66
AAE1990 <anton.akhmadeev@yandex.com>
2026-06-30 15:25:14 +0500

    fix: bypass session domain validator with none value

--- nestjs-server/src/main.ts
+++ nestjs-server/src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
       resave: true,
       saveUninitialized: false,
       cookie: {
-        domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+        domain: config.get<string>('SESSION_DOMAIN') === 'none' ? undefined : config.get<string>('SESSION_DOMAIN'),
         maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
         httpOnly: parseBoolean(
           config.getOrThrow<string>('SESSION_HTTP_ONLY')
