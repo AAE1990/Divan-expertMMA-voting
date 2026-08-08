9d34cdca8833c7db8af7c8c83670395367bbd0ad
AAE1990 <anton.akhmadeev@yandex.ru>
2026-08-08 17:05:57 +0500

    fix(frontend): sync api env variable name and fix compose indentation
    - Change NEXT_PUBLIC_SERVER_URL to NEXT_PUBLIC_API_URL in instance.api.ts
    
    - Fix indentation of build args for frontend service in docker-compose.yml

--- nextjs-client/src/shared/api/instance.api.ts
+++ nextjs-client/src/shared/api/instance.api.ts
@@ -1,7 +1,7 @@
 import { FetchClient } from "../utils"
 
 // 1. Берем URL из .env (теперь с NEXT_PUBLIC_)
-const rawUrl = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:4000';
+const rawUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
 
 // 2. Гарантируем, что Base URL ЗАКОНЧИТСЯ на слэш
 const baseUrl = rawUrl.endsWith('/') ? rawUrl : `${rawUrl}/`;
