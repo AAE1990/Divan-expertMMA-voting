bfed15786b0db96cb3dd3f1cc581aea124b423a1
AAE1990 <anton.akhmadeev@yandex.ru>
2026-09-19 00:29:51 +0500

    backap findService method

--- nestjs-server/src/auth/auth.service.ts
+++ nestjs-server/src/auth/auth.service.ts
@@ -132,15 +132,9 @@ export class AuthService {
 
         const profile = await providerInstance.findUserByCode(code)
 
-        // const account = await this.prismaService.account.findFirst({
-        //     where: {
-        //         id: profile.id,
-        //         provider: profile.provider
-        //     }
-        // })
         const account = await this.prismaService.account.findFirst({
             where: {
-                providerAccountId: profile.id,
+                id: profile.id,
                 provider: profile.provider
             }
         })
